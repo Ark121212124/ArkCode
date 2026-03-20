4b978a77b0b1b34e50ffefac13e3c3790686087f
Ark121212124 <Arktiko12@gmail.com>
2026-03-20 10:58:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const http = require("http");
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Сервер работает 🚀");
+  res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+  res.end("Привет сервер 🚀");
 });
 
 server.listen(port, () => {
